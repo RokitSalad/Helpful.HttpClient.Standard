235988892176549fa944ac13498a05f1e044dd52
rokitsalad <peterdobbs@hotmail.com>
2020-06-15 21:27:07 +1000

    initial shot at the middleware

--- Helpful.HttpClient.Standard/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Helpful.HttpClient.Standard
-{
-    public class Class1
-    {
-    }
-}
---
+++ Helpful.HttpClient.Standard/HeaderPersistenceMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Helpful.Logging.Standard;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Primitives;
+
+namespace Helpful.HttpClient.Standard
+{
+    public class HeaderPersistenceMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly string[] _headersToPersist;
+
+        public HeaderPersistenceMiddleware(RequestDelegate next, params string[] headersToPersist)
+        {
+            _next = next;
+            _headersToPersist = headersToPersist;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            foreach (var header in _headersToPersist)
+            {
+                if (context.Response.Headers.TryGetValue(header, out StringValues headerValue))
+                {
+                    LoggingContext.Set(header, headerValue);
+                }
+            }
+
+            await _next(context);
+        }
+    }
+
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseHeaderPersistenceMiddleware(this IApplicationBuilder builder, params string[] headersToPersist)
+        {
+            return builder.UseMiddleware<HeaderPersistenceMiddleware>(headersToPersist);
+        }
+    }
+}
